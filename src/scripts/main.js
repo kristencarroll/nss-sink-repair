4f3a7431c24638b4b68c04178ec8e1aaa6b09bc0
Kristen Carroll <carroll.kristen@gmail.com>
2022-02-12 16:54:02 -0600

    first commit ..

---
+++ src/scripts/main.js
@@ -0,0 +1,26 @@
+import { SinkRepair } from "./SinkRepair.js"
+
+
+const mainContainer = document.querySelector("#container")
+
+const render = () => {
+    mainContainer.innerHTML = SinkRepair()
+}
+
+render()
+
+import { fetchRequests } from "./dataAccess.js"
+import { SinkRepair } from "./SinkRepair.js"
+
+
+const mainContainer = document.querySelector("#container")
+
+const render = () => {
+    fetchRequests().then(
+        () => {
+            mainContainer.innerHTML = SinkRepair()
+        }
+    )
+}
+
+render()
